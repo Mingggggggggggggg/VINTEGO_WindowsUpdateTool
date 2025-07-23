31993d0e23095fb10006d86fdb0e4ac273314fe9
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-07-23 08:27:02 +0200

    Füge Script zum Start hinzu

---
+++ appScript.ps1
@@ -0,0 +1,75 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]$downloadPath = $env:quellverzechnis,
+    [Parameter()]
+    [String]$fileName = $env:dateinamen,
+    [Parameter()]
+    [String]$targetPath = $env:zielverzeichnis
+)
+
+# Standardwerte
+$appPath = "C:\VINTEGO-Technik\Tools"
+$appUrl = "https://testdownload.de/Windows10-UpdateTool.exe"
+$exeFile = Join-Path -Path $appPath -ChildPath "Windows10-UpdateTool.exe"
+$logPath = "C:\Users\VINTEGO\Logs\log.txt"
+
+begin {
+
+  function runExec {
+    $cmdArgs = @()
+    $cmdArgs += $downloadPath
+
+    if ($fileName) {
+      $cmdArgs += "--fileName"
+      $cmdArgs += $fileName
+    } else {
+      Write-Output "Kein Dateiname angegeben. Suche nach genau einer ISO im Quellverzeichnis."
+    }
+
+    if ($targetPath) {
+      $cmdArgs += "--targetPath"
+      $cmdArgs += $targetPath
+    } else {
+      Write-Output "Kein Zielpfad angegeben. Verwende Standardpfad C:\Users\VINTEGO\Installer."
+    }
+
+    Write-Output "Starte Windows10-UpdateTool Anwendung. Dies kann einen Moment dauern."
+    & $exeFile @cmdArgs
+  }
+
+  function readLogs {
+    if (Test-Path $logPath) {
+      $log = Get-Content $logPath
+      $result = $log -join "`n"
+      return $result
+    } else {
+      return "Kein Log gefunden."
+    }
+  }
+
+  function getApp {
+    if (Test-Path -Path $exeFile) {
+      Write-Output "Anwendung bereits vorhanden."
+      runExec
+    } else {
+      if (-not (Test-Path $appPath)) {
+        New-Item -Path $appPath -ItemType Directory | Out-Null
+      }
+      Write-Output "Lade Anwendung von $appUrl herunter."
+      Invoke-WebRequest -Uri $appUrl -OutFile $exeFile
+
+      if (Test-Path $exeFile) {
+        Write-Output "Anwendung erfolgreich heruntergeladen."
+        runExec
+      } else {
+        Write-Output "Fehler: Anwendung konnte nicht heruntergeladen werden."
+        exit 1
+      }
+    }
+  }
+
+  getApp
+  ninja-property-set windows10updatetoollog (readLogs)
+  Write-Output "Skript beendet."
+}
